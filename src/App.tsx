f647f7810620769779f82d95877e87ef8dd07903
BerrySeriousCoder <harsh100xdev.work@gmail.com>
2025-01-28 20:07:59 +0530

    added lenis for smooth scroll

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,30 @@ import Footer from './components/footer/footer'
 import PricingPage from './pages/pricing'
 import AboutUs from './pages/aboutus'
 import ContactUs from './pages/contactus'
-
+import { useEffect } from 'react'
+import Lenis from 'lenis'
 function App() {
+  useEffect(() => {
+   
+    const lenis = new Lenis({
+      duration: 1.7, 
+      easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // Easing function
+      touchMultiplier: 2,
+    });
+
+   
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+  
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
 
   return (
     <>
